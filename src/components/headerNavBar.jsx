2c300d0b49ec92c521b7ad7f908983b55e07498a
npmackay <noah.mackay-lachevrotiere@student.sl.on.ca>
2024-05-05 11:14:47 -0400

    fixed title and now create posting button only appears when /marketplace page is hit

--- src/components/headerNavBar.jsx
+++ src/components/headerNavBar.jsx
@@ -1,31 +1,43 @@
-import { useState } from "react";
+import { useState , useEffect } from "react";
 import { AppBar, Toolbar, Typography, Button } from "@mui/material";
-import { useNavigate } from "react-router-dom";
+import { useNavigate , useLocation} from "react-router-dom";
 import CreatePostingModal from "../createPostingModal";
 import { useCurrentUser } from "../components/contexts/currentUserContext";
 function HeaderNavBar() {
   const [openCreatePosting, setOpenCreatePosting] = useState(false);
   const { currentUser: user } = useCurrentUser();
+   let location = useLocation();
   let navigate = useNavigate();
+    useEffect(() => {
+      setCurrentPage(location.pathname);
+    }, [location]);
   const handleLogout = () => {
     console.log("Logout");
     navigate("/");
   };
+  const [currentPage, setCurrentPage] = useState("");
 
   return (
     <AppBar position="static">
       <Toolbar sx={{ display: "flex", justifyContent: "space-evenly" }}>
         <Typography variant="h4">Welcome {user?.currentUser} </Typography>
-        <Button color="inherit" onClick={() => setOpenCreatePosting(true)}>
-          Create Posting
-        </Button>
-
-        <Button color="inherit" onClick={() => navigate("/marketplace")}>
-          MarketPlace
-        </Button>
         <Button color="inherit" onClick={() => navigate("/homepage")}>
           Homepage
         </Button>
+        <Button
+          color="inherit"
+          onClick={() => {
+            setCurrentPage("/marketplace");
+            navigate("/marketplace");
+          }}
+        >
+          MarketPlace
+        </Button>
+        {currentPage === "/marketplace" && (
+          <Button color="inherit" onClick={() => setOpenCreatePosting(true)}>
+            Create Posting
+          </Button>
+        )}
         <Button color="inherit" onClick={handleLogout}>
           Logout
         </Button>
--- src/homepage.jsx
+++ src/homepage.jsx
@@ -14,6 +14,7 @@ import React, { useEffect, useState } from "react";
 function Homepage() {
   const [itemPostings, setItemPostings] = useState([]);
 
+  
   const supportedGames = [
     { id: 1, name: "League of Legends", backgroundImg: "" },
     { id: 2, name: "Valorant", backgroundImg: "" },
