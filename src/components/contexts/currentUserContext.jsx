07ecfad8b6d5a8fadb9649542c1b532191e17962
npmackay <noah.mackaylachevrotierr@Student.SL.On.Ca>
2024-05-26 12:08:03 -0400

    added persistent user info through refresh

--- src/components/contexts/currentUserContext.jsx
+++ src/components/contexts/currentUserContext.jsx
@@ -1,11 +1,22 @@
-import { useContext, createContext, useState } from 'react';
+import { useContext, createContext, useState } from "react";
 
 const CurrentUserContext = createContext();
 
 export function CurrentUserProvider({ children }) {
-  const [currentUser, setCurrentUser] = useState(null);
+  const [currentUser, setCurrentUser] = useState(() => {
+    const localUser = localStorage.getItem("currentUser");
+    return localUser ? JSON.parse(localUser) : null;
+  });
+
+  const setAndPersistUser = (user) => {
+    setCurrentUser(user);
+    localStorage.setItem("currentUser", JSON.stringify(user));
+  };
+
   return (
-    <CurrentUserContext.Provider value={{ currentUser, setCurrentUser }}>
+    <CurrentUserContext.Provider
+      value={{ currentUser, setCurrentUser: setAndPersistUser }}
+    >
       {children}
     </CurrentUserContext.Provider>
   );
---
+++ todo.md
@@ -0,0 +1,25 @@
+# To-Do List
+
+## Task 1
+
+- [ ] persistent user info after refresh
+  - [ ] Look into local storage with token
+  - [ ] Outline
+  - [ ] Gather resources
+- [ ] Plan task 1
+
+## Task 2
+
+- [ ] Plan task 1
+  - [ ] Research
+  - [ ] Outline
+  - [ ] Gather resources
+- [ ] Plan task 1
+
+## Task 3
+
+- [ ] Plan task 1
+  - [ ] Research
+  - [ ] Outline
+  - [ ] Gather resources
+- [ ] Plan task 1
